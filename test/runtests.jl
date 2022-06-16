f0d9d8751506edff9edb30ef2bbf1a258299eeab
Pierre Haessig <pierre.haessig@crans.org>
2022-06-16 11:28:16 +0200

    add test for battery economics

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,7 +21,8 @@ end
 
 @testset "Economics: PV" begin
     lifetime_mg = 30
-    discount_rate = 0.05
+    proj0 = Project(lifetime_mg, 0.00, 1.) # no discount
+    proj5 = Project(lifetime_mg, 0.05, 1.) # 5% discount
 
     power_rated_PV = 10 # kW
     fPV = 1.
@@ -33,13 +34,43 @@ end
     salvage_cost_PV = 800.
     lifetime_PV = 20
 
-    project0 = Project(lifetime_mg, 0.00, 1.) # no discount
-    project5 = Project(lifetime_mg, 0.05, 1.) # 5% discount
-
     pv = Photovoltaic(power_rated_PV, fPV, IT, IS, investiment_cost_PV, om_cost_PV, replacement_cost_PV, salvage_cost_PV, lifetime_PV)
 
-    @test annual_costs(pv, project0) ==                   [24000.0,  10000.0, 6000.0, 12000.0, 4000.0]
-    @test round.(annual_costs(pv, project5); digits=2) == [16671.65, 10000.0, 3074.49, 4522.67, 925.51]
+    @test annual_costs(pv, proj0) ==                   [24000.0,  10000.0, 6000.0, 12000.0, 4000.0]
+    @test round.(annual_costs(pv, proj5); digits=2) == [16671.65, 10000.0, 3074.49, 4522.67, 925.51]
+end
+
+@testset "Economics: Battery" begin
+    lifetime_mg = 25 # just a bit more than the battery
+    proj0 = Project(lifetime_mg, 0.00, 1.) # no discount
+    proj5 = Project(lifetime_mg, 0.05, 1.) # 5% discount
+    
+    energy_initial = 0.
+    energy_max = 7
+    energy_min = 0
+    power_min = -1.0*energy_max
+    power_max = +1.0*energy_max
+    loss = 0.05
+    investiment_cost_BT = 100.
+    om_cost_BT = 10
+    replacement_cost_BT = 90.
+    salvage_cost_BT = 80
+    lifetime_BT = 20
+    lifetime_thrpt = 3000
+
+    batt = Battery(energy_initial, energy_max, energy_min, power_min, power_max, loss, investiment_cost_BT, om_cost_BT, replacement_cost_BT, salvage_cost_BT, lifetime_BT, lifetime_thrpt)
+
+    # Fake operation data: 1k cycles/year, 
+    aggr0C  = OperVarsAggr(0., 0., 0., 0., 0., 0., 0.,    0., 0., 0.)
+    aggr100C  = OperVarsAggr(0., 0., 0., 0., 0., 0., 0.,  100., 0., 0.) # not enough cycles to reduce the lifetime
+    aggr1kC = OperVarsAggr(0., 0., 0., 0., 0., 0., 0.,  300.0*energy_max, 0., 0.) # lifetime reduced to 3000/300 = 10 yr
+
+    # no discount, with increasing amount of cycling
+    @test annual_costs(batt, proj0, aggr0C)   == [2660.0, 700.0, 1750.0,  630.0, 420.0]
+    @test annual_costs(batt, proj0, aggr100C) == [2660.0, 700.0, 1750.0,  630.0, 420.0]
+    @test annual_costs(batt, proj0, aggr1kC) ==  [3430.0, 700.0, 1750.0, 1260.0, 280.0]
+    # with discount
+    @test round.(annual_costs(batt, proj5, aggr0C); digits=2) ==  [1799.99, 700.0, 986.58, 237.44, 124.03]
 end
 
 @testset "Economics: MG" begin
@@ -49,8 +80,8 @@ end
     discount_rate = 0.05
     timestep = 1 # h
 
-    project0 = Project(lifetime, 0, timestep)
-    project5 = Project(lifetime, discount_rate, timestep)
+    proj0 = Project(lifetime, 0, timestep)
+    proj5 = Project(lifetime, discount_rate, timestep)
 
     power_rated_PV = 6000 # kW
     fPV = 1.
@@ -92,8 +123,8 @@ end
 
     dieselgenerator = DieselGenerator(power_rated_DG, min_load_ratio, F0, F1, fuel_cost, investiment_cost_DG, om_cost_DG, replacement_cost_DG, salvage_cost_DG, lifetime_DG)
 
-    mg0 = Microgrid(project0, Pload, dieselgenerator, battery, [photovoltaic]);
-    mg5 = Microgrid(project5, Pload, dieselgenerator, battery, [photovoltaic]);
+    mg0 = Microgrid(proj0, Pload, dieselgenerator, battery, [photovoltaic]);
+    mg5 = Microgrid(proj5, Pload, dieselgenerator, battery, [photovoltaic]);
 
     # Bypass of the operation simulation + aggregation:
     opervarsaggr = OperVarsAggr(6.774979e6, 0, 0, 0, 0.0, 3327, 670880.8054857135, 1.881753568922309e6, 4569.3, 58.74028997693115)
