ff47ec76223607ee87e8d905104b2f03246a5809
Pierre Haessig <pierre.haessig@crans.org>
2023-07-13 19:41:05 +0200

    Fix aggregation:
    - matches the newly changed OperStats structure
    - cleaner code

--- src/operation.jl
+++ src/operation.jl
@@ -10,6 +10,9 @@ struct OperationTraj{T<:Real}
     power_net_load::Vector{T}
     "Unmet load/Load shedding power at each time instant (kW)"
     power_shedding::Vector{T}
+    # non dispatchable (renewables)
+    "renewable power potential (before spillage) (kW)"
+    Prenew_pot::Vector{T}
     # diesel generator
     "Diesel generator power at each time instant (kW)"
     Pgen::Vector{T}
@@ -51,7 +54,7 @@ struct OperationStats
     gen_energy
     "cumulated operating hours of the dispatchable generator (h/y)"
     gen_hours
-    "Diesel consumption in one year (L)"
+    "fuel consumption (L/y)"
     gen_fuel
 
     # Energy storage (e.g. battery) statistics
@@ -134,199 +137,120 @@ function operation(mg::Microgrid)
         end
     end
 
-    oper_traj = OperationTraj(power_net_load, Pshed, Pgen, Ebatt, Pbatt, Pbatt_dmax, Pbatt_cmax, Pcurt)
+    oper_traj = OperationTraj(power_net_load, Pshed, Prenew_pot, Pgen, Ebatt, Pbatt, Pbatt_dmax, Pbatt_cmax, Pcurt)
     return oper_traj
 end
 
 """
-    aggregation(mg::Microgrid, oper_traj::OperationTraj)
+    aggregation(mg::Microgrid, oper_traj::OperationTraj, ε::Real=1.0)
 
-Return the aggregated operation statistics `OperationStats` for the microgrid `mg` and
-the hourly operation variables `OperationTraj`.
+Aggregates operation time series `oper_traj` into yearly statistics
+for the the microgrid `mg` (returned as an `OperationStats` object).
+
+Discontinuous statistics can optionally be relaxed (smoothed)
+using the relaxation parameter `ε`:
+- 0.0 means no relaxation (default value)
+- 1.0 yields the strongest relaxation
+
+when using relaxation, a value between 0.05 and 0.30 is suggested
 """
-function aggregation(mg::Microgrid, oper_traj::OperationTraj)
-    # variables initialization
-    nHours = 0
-    vFuel  = 0
-    Eserv = 0
-    delestTimeCurrent = 0
-    delestTimeMax = 0
-    annualThrpt = 0.0
-    for i = 1 : length(oper_traj.Pgen)
-        # diesel generator
-        if oper_traj.Pgen[i] > 0 #DG on
-            nHours = nHours + 1 # incremention of the number of hours
-            fuelConsump = mg.dieselgenerator.F0 * mg.dieselgenerator.power_rated + mg.dieselgenerator.F1 * oper_traj.Pgen[i] #[L.h-1]  F = F0*Ygen + F1*Pgen
-        else
-            fuelConsump = 0 # DG off [L.h-1]
-        end
-        vFuel = vFuel + fuelConsump*mg.project.timestep # [L]
+function aggregation(mg::Microgrid, oper_traj::OperationTraj, ε::Real=1.0)
+    ### Retrieve parameters
+    dt = mg.project.timestep
+    nsteps = length(mg.load)
 
-        # battery
-        annualThrpt = annualThrpt + abs(oper_traj.Pbatt[i])*mg.project.timestep #battery throughtput (charging & dicharging)
+    ### Compute simple yearly statistics (sum, max...):
+    # Load statistics
+    load_energy = sum(mg.load) * dt # kWh/y
+    shed_energy = sum(oper_traj.power_shedding) * dt # kWh/y
+    served_energy = load_energy - shed_energy # kWh/y
+    shed_max = max(oper_traj.power_shedding) # kW
+    shed_rate = shed_energy / load_energy
 
-        # load - energy served - energy provided (served in one year) [kWh]
-        Eserv = Eserv + (mg.power_load[i] - oper_traj.power_shedding[i]) * mg.project.timestep
+    # Dispatchable generator statistics
+    gen_energy = sum(oper_traj.Pgen) * dt # kWh/y
 
-        # load - calculation of the Max shedding duration
-        if oper_traj.power_shedding[i] > 0 && i!=length(oper_traj.Pgen)
-            delestTimeCurrent = delestTimeCurrent + 1
-        else
-            if delestTimeCurrent > delestTimeMax
-                delestTimeMax = delestTimeCurrent
+    # Energy storage (e.g. battery) statistics
+    pos(x) = x >= 0.0 ? x : 0.0 # positive part
+    storage_char_energy = sum(pos.(oper_traj.Pbatt)) * dt # kWh/y
+    storage_dis_energy = sum(pos.(-oper_traj.Pbatt)) * dt # kWh/y
+    storage_cycles = (storage_char_energy + storage_dis_energy) /
+                     (2*mg.storage.energy_rated) # cycles/y
+    Efin = oper_traj.Ebatt[end]
+    Eini = oper_traj.Ebatt[1]
+    storage_loss_energy = storage_char_energy - storage_dis_energy -
+                          (Efin - Eini) # kWh/y
+
+    # Non-dispatchable (typ. renewables) sources statistics
+    spilled_energy = sum(oper_traj.power_curtailment) * dt # kWh/y
+    spilled_max = max(oper_traj.power_curtailment) # kW
+    renew_potential_energy = sum(oper_traj.Prenew_pot) * dt # kWh/y
+    spilled_rate = spilled_energy / renew_potential_energy
+    renew_energy = renew_potential_energy - spilled_energy
+    renew_rate = 1 - gen_energy/served_energy
+
+    ### Iterative computation of more complex yearly statistics
+
+    # Initialization of integrators:
+    shed_hours = 0.0 # h/y
+    shed_duration_max = 0.0 # h
+    gen_hours = 0.0 # h/y
+    gen_fuel = 0.0 # L/y
+
+    # auxilliary counter:
+    shed_duration = 0.0; # duration of current load shedding event (h)
+    Pload_avg = load_energy/(nsteps*dt) # kW
+
+    for i = 1:nsteps
+        # Dispatchable generator : operating hours and fuel consumption
+        Pgen = oper_traj.Pgen[i]
+        Pgen_max = mg.generator.power_rated
+        if Pgen > 0.0 # generator ON
+            Pgen_norm = Pgen / (ε * Pgen_max) # can be Inf e.g. for ε=0.0
+            if Pgen_norm <= 1.0
+                # relaxation of discontinuous generator statistics for small Pgen
+                gen_hours += Pgen_norm * dt
+                fuel_rate = Pgen_norm * mg.generator.fuel_intercept * Pgen_max +
+                            mg.generator.fuel_slope * Pgen # (L/h)
+            else
+                gen_hours += dt
+                fuel_rate = mg.generator.fuel_intercept * Pgen_max +
+                            mg.generator.fuel_slope * Pgen # (L/h)
             end
-            delestTimeCurrent = 0 # Ré-initialisation de la durée de délestage
+            gen_fuel += fuel_rate * dt
+        end
+
+        # Load shedding: shedding duration and maximum shedding duration
+        Pshed = oper_traj.power_shedding[i]
+        if Pshed > 0.0
+            Pshed_norm = Pshed / (ε * Pload_avg) # can be Inf e.g. for ε=0.0
+            if Pshed_norm <= 1.0
+                # relaxation of discontinuous load statistics for small Pshed
+                shed_hours += Pshed_norm * dt
+                shed_duration += Pshed_norm * dt
+            else
+                shed_hours += dt
+                shed_duration += dt
+            end
+            # keep track of maximum shedding duration:
+            shed_duration_max = max(shed_duration, shed_duration_max)
+        else
+            # reset shedding duration counter:
+            shed_duration = 0.0
         end
     end
 
-    # load
-    Pshed_max = maximum(oper_traj.power_shedding)    # max shedding
-    shed_time_max = delestTimeMax
-    shed_total_kwh = sum(oper_traj.power_shedding) * mg.project.timestep # [kWh]
-    shed_rate =  (sum(oper_traj.power_shedding) * mg.project.timestep) / (sum(mg.power_load)*mg.project.timestep) # [0,1]
-    # battery
-    annualThrpt = annualThrpt/2    # (charging and discharging cycle)
-    # renewables sources
-    Pcurt_max = maximum(oper_traj.power_curtailment)    # max curtailment
-    renew_rate = (1 - sum(oper_traj.Pgen)/Eserv) * 100 # d'insertion ENR insertion Rate
-    # outputs
-    oper_stats = OperationStats(Eserv, Pshed_max, shed_time_max, shed_total_kwh, shed_rate, nHours, vFuel, annualThrpt, Pcurt_max, renew_rate)
-
-    # TODO - parei aqui
-    # load
-    # operVars.delestSumHours = sum(operVars.loaddelestRealHours); #[h]
-    # operVars.delestRateHours =  100*sum(operVars.loaddelestRealHours) / length(mgDef.load); # [%]
-    # operVars.serviceRate = 100 - operVars.load.delestRate; # service rate in term of Energy (%)
-    # operVars.serviceRateHours = 100 - operVars.load.delestRateHours; #  service rate in term of duration[%]
-
+    # Outputs
+    oper_stats = OperationStats(
+        # Load statistics
+        served_energy, shed_energy, shed_max, shed_hours, shed_duration_max, shed_rate,
+        # Dispatchable generator statistics
+        gen_energy, gen_hours, gen_fuel,
+        # Energy storage (e.g. battery) statistics
+        storage_cycles, storage_char_energy, storage_dis_energy, storage_loss_energy,
+        # Non-dispatchable (typ. renewables) sources statistics
+        spilled_energy, spilled_max, spilled_rate,
+        renew_potential_energy, renew_energy, renew_rate
+    )
     return oper_stats
 end
-
-"""
-    aggregation(mg::Microgrid, oper_traj::OperationTraj, ε)
-
-When a relaxation factor `ε` is given, return the aggregated operation statistics
-`OperationStats` with a linear relaxation of the diesel generator annual operation
-hours `OperVarsAggr.nHours`.
-"""
-function aggregation(mg::Microgrid, oper_traj::OperationTraj, ε)
-    # variables initialization
-    nHours = 0.
-    vFuel  = 0
-    Eserv = 0
-    delestTimeCurrent = 0
-    delestTimeMax = 0
-    annualThrpt = 0
-    for i = 1 : length(oper_traj.Pgen)
-        x = oper_traj.Pgen[i]/mg.dieselgenerator.power_rated
-        # diesel generator
-        if oper_traj.Pgen[i] > 0 && x <= ε   #DG on
-            nHours = nHours + x/ε # incremention of the number of hours
-            fuelConsump = mg.dieselgenerator.F0 * mg.dieselgenerator.power_rated + mg.dieselgenerator.F1 * oper_traj.Pgen[i] #[L.h-1]  F = F0*Ygen + F1*Pgen
-        elseif oper_traj.Pgen[i] > 0 && x > ε   #DG on
-            nHours = nHours + 1 # incremention of the number of hours
-            fuelConsump = mg.dieselgenerator.F0 * mg.dieselgenerator.power_rated + mg.dieselgenerator.F1 * oper_traj.Pgen[i] #[L.h-1]  F = F0*Ygen + F1*Pgen
-        else
-            fuelConsump = 0 # DG off [L.h-1]
-        end
-        vFuel = vFuel + fuelConsump*mg.project.timestep # [L]
-
-        # battery
-        annualThrpt = annualThrpt + abs(oper_traj.Pbatt[i])*mg.project.timestep #battery throughtput (charging & dicharging)
-
-        # load - energy served - energy provided (served in one year) [kWh]
-        Eserv = Eserv + (mg.power_load[i] - oper_traj.power_shedding[i]) * mg.project.timestep
-
-        # load - calculation of the Max shedding duration
-        if oper_traj.power_shedding[i] > 0 && i!=length(oper_traj.Pgen)
-            delestTimeCurrent = delestTimeCurrent + 1
-        else
-            if delestTimeCurrent > delestTimeMax
-                delestTimeMax = delestTimeCurrent
-            end
-            delestTimeCurrent = 0 # Ré-initialisation de la durée de délestage
-        end
-    end
-
-    # load
-    Pshed_max = maximum(oper_traj.power_shedding)    # max shedding
-    shed_time_max = delestTimeMax
-    shed_total_kwh = sum(oper_traj.power_shedding) * mg.project.timestep # [kWh]
-    shed_rate =  (sum(oper_traj.power_shedding) * mg.project.timestep) / (sum(mg.power_load)*mg.project.timestep) * 100 # (%)
-    # battery
-    annualThrpt = annualThrpt/2    # (charging and discharging cycle)
-    # renewables sources
-    Pcurt_max = maximum(oper_traj.power_curtailment)    # max curtailment
-    renew_rate = (1 - sum(oper_traj.Pgen .* mg.project.timestep)/Eserv) * 100 # d'insertion ENR insertion Rate
-    # outputs
-    oper_stats = OperationStats(Eserv, Pshed_max, shed_time_max, shed_total_kwh, shed_rate, nHours, vFuel, annualThrpt, Pcurt_max, renew_rate)
-
-    # TODO - parei aqui
-    # load
-    # operVars.delestSumHours = sum(operVars.loaddelestRealHours); #[h]
-    # operVars.delestRateHours =  100*sum(operVars.loaddelestRealHours) / length(mgDef.load); # [%]
-    # operVars.serviceRate = 100 - operVars.load.delestRate; # service rate in term of Energy (%)
-    # operVars.serviceRateHours = 100 - operVars.load.delestRateHours; #  service rate in term of duration[%]
-
-    return oper_stats
-end
-
-# # With the number of DG operation hours relaxed - exponential
-# function aggregation(mg::Microgrid, opervarstraj::OperVarsTraj, ε)
-#     # variables initialization
-#     nHours = 0.
-#     vFuel  = 0
-#     Eserv = 0
-#     delestTimeCurrent = 0
-#     delestTimeMax = 0
-#     annualThrpt = 0
-#     for i = 1 : length(opervarstraj.Pgen)
-#         x = opervarstraj.Pgen[i]/mg.dieselgenerator.power_rated
-#         # diesel generator
-#         if opervarstraj.Pgen[i] > 0   #DG on
-#             nHours = nHours + exp(-ε * x) # incremention of the number of hours
-#             fuelConsump = mg.dieselgenerator.F0 * mg.dieselgenerator.power_rated + mg.dieselgenerator.F1 * opervarstraj.Pgen[i] #[L.h-1]  F = F0*Ygen + F1*Pgen
-#         else
-#             fuelConsump = 0 # DG off [L.h-1]
-#         end
-#         vFuel = vFuel + fuelConsump*mg.project.timestep # [L]
-
-#         # battery
-#         annualThrpt = annualThrpt + abs(opervarstraj.Pbatt[i])*mg.project.timestep #battery throughtput (charging & dicharging)
-
-#         # load - energy served - energy provided (served in one year) [kWh]
-#         Eserv = Eserv + (mg.load[i] - opervarstraj.Pshed[i]) * mg.project.timestep
-
-#         # load - calculation of the Max shedding duration
-#         if opervarstraj.Pshed[i] > 0 && i!=length(opervarstraj.Pgen)
-#             delestTimeCurrent = delestTimeCurrent + 1
-#         else
-#             if delestTimeCurrent > delestTimeMax
-#                 delestTimeMax = delestTimeCurrent
-#             end
-#             delestTimeCurrent = 0 # Ré-initialisation de la durée de délestage
-#         end
-#     end
-
-#     # load
-#     Pshed_max = maximum(opervarstraj.Pshed)    # max shedding
-#     shed_time_max = delestTimeMax
-#     shed_total_kwh = sum(opervarstraj.Pshed) * mg.project.timestep # [kWh]
-#     shed_rate =  (sum(opervarstraj.Pshed) * mg.project.timestep) / (sum(mg.load)*mg.project.timestep) # [0,1]
-#     # battery
-#     annualThrpt = annualThrpt/2    # (charging and discharging cycle)
-#     # renewables sources
-#     Pcurt_max = maximum(opervarstraj.Pcurt)    # max curtailment
-#     renew_rate = (1 - sum(opervarstraj.Pgen)/Eserv) * 100 # d'insertion ENR insertion Rate
-#     # outputs
-#     opervarsaggr = OperVarsAggr(Eserv, Pshed_max, shed_time_max, shed_total_kwh, shed_rate, nHours, vFuel, annualThrpt, Pcurt_max, renew_rate)
-
-#     # TODO - parei aqui
-#     # load
-#     # operVars.delestSumHours = sum(operVars.loaddelestRealHours); #[h]
-#     # operVars.delestRateHours =  100*sum(operVars.loaddelestRealHours) / length(mgDef.load); # [%]
-#     # operVars.serviceRate = 100 - operVars.load.delestRate; # service rate in term of Energy (%)
-#     # operVars.serviceRateHours = 100 - operVars.load.delestRateHours; #  service rate in term of duration[%]
-
-#     return opervarsaggr
-# end
